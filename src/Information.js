6452dffd348eab58e35b5af1d4ce441851951c7f
JaceRogers12 <jacerogers12@gmail.com>
2024-04-15 19:01:55 -0600

    added racial data

--- src/Information.js
+++ src/Information.js
@@ -5,7 +5,9 @@ export const information = {
             title: "",
             subheading: "",
             image: null,
-            text: `Ramaria is a fantasy world full of unique creatures, fascinating magic, and interesting history. At the moment, however, both Ramaria and this website (as well as the Game and Stories) are works in progress. For now, I recommend you look in the Races section for the diverse types of people within my world.`,
+            text: [
+                `Ramaria is a fantasy world full of unique creatures, fascinating magic, and interesting history. At the moment, however, both Ramaria and this website (as well as the Game and Stories) are works in progress. For now, I recommend you look in the Races section for the diverse types of people within my world.`,
+            ],
         }},
     Locations: {
         Main: {
@@ -136,19 +138,27 @@ export const information = {
             title: "People of Ramaria",
             subheading: "",
             image: null,
-            text: "",
+            text: [
+                `Though Ramaria is a fantasy world with different races of humanoid people, those people are not elves, dwarves, and fairies like in many other worlds. I wanted to make races that were very different from humans, rather than just having pointy ears or a stout stature. (I'm oversimplifying, and I do enjoy typical fantasy races, but I love making new, unique people even more.) So, these races that I've made try to be unique in intriguing ways while not feeling alien to people who live in a world where some of the most prominent features that set us apart are height and skin tone.`,
+            ],
         },
         Human: {
             title: "Humans",
             subheading: "",
             image: null,
-            text: "",
+            text: [`The humans of Ramaria are nearly the same as the humans of Earth. Compared to the other races of Ramaria, humans are balanced, adaptable individuals that fit in anywhere, with no speical racial features that set them apart from others.`,
+                `Humans are widespread, with a few of their own countries throughout the world. They also are frequently mixed in with countries dominated by other races. Wherever you go in Ramaria, there are usually humans to be found somewhere nearby.`,
+            ],
         },
         Ramu: {
             title: "Ramu",
             subheading: "",
             image: null,
-            text: "",
+            text: [
+                `The ramu are the most animalistic race in Ramaria. They have fur coats, tails, flat snouts, claws, and digitigrade legs. Their ears are long and on the tops of their heads, rather than on the sides. They often have very divese size ranges, anywhere from 4 feet to 7 feet tall.`,
+                `The ramu also typically live nomadic lifestyles, moving from place to place. Freedom to move, choose, and express themselves are all things they value highly. A spirit of adventure and discovery calls to them, driving them to roam, even dispite the dangers of the wilds.`,
+                `Ramu are typically healthy and strong, allowing them to live for as long as 120 years. However, due to the unstable nature of life on the move and the threats of the world away from civilization, their typical lifespans are usually much shorter; most of them die in their 70s. Maturity progresses quickly for raman adolescents, and can happen at any time throughout their teenage years. Some youths are fully matured when others are just beginning to start the process. The effects of old age usually begin to occur when they reach their late 60s or 70s. Old ramu that can't keep up with their nomadic tribes will often join a nearby friendly settlement of another community to rest from a life of travel.`,
+            ],
         },
         Jaiyu: {
             title: "Jaiyu",
