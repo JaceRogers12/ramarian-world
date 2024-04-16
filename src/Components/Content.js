fe246a10c7f5bfb2f34f5a8a5cf87c1358be29a7
JaceRogers12 <jacerogers12@gmail.com>
2024-04-15 18:00:37 -0600

    organized the information and how it will be displayed in Content

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,28 +1,33 @@
 import React from "react";
 import {information} from "../Information";
 
-function Home (props) {
+function Content (props) {
     const {info, topic, topicChange} = props;
-    
+    let current = information[info][topic];
+
     const optionsMaker = () => {
         let result = [];
         let count = 0;
         for (let property in information[info]) {
-            result.push(<option value={property} key={count++}>{property}</option>)
+            result.push(<option value={property} key={count++}>{information[info][property]["title"]}</option>)
         }
         return result;
     }
     
-    //console.log(optionsMaker());
+    //console.log();
     return (
         <div className="content" >
             <h2>{info}</h2>
-            <span >Topics: </span>
-            <select name="topic" value={topic} onChange={event => topicChange(event)} >
-                {optionsMaker()}
-            </select>
+            {current["title"]
+                && <select name="topic" value={topic} onChange={event => topicChange(event)} >
+                    {optionsMaker()}
+                </select>
+            }
+            {current["subheading"] && <p className="subheading" >{current["subheading"]}</p>}
+            {current["image"] && <img alt={`${current["title"]} Image`} />}
+            <p className="text" >{current["text"]}</p>
         </div>
     )
 }
 
-export default Home;
+export default Content;
